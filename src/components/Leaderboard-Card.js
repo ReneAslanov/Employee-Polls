3cc2588bce5af43240763f873e645151e3bf2711
KenShyNoShana <ReneAslanov02@gmail.com>
2022-06-09 14:09:29 +0200

    refactor: props are now destructured

--- src/components/Leaderboard-Card.js
+++ src/components/Leaderboard-Card.js
@@ -1,20 +1,20 @@
-function LeaderboardCard(props)
+function LeaderboardCard({user})
 {
     return(
-        <div className="leaderboard-items-wrapper" key={props.user.id}>
+        <div className="leaderboard-items-wrapper" key={user.id}>
             <div className="leaderboard-user-name leaderboard-item">
                 <div className="leaderboard-user-wrapper">
-                    <h3 className="leaderboard-userName">{props.user.name}</h3>
-                    <h4 className="leaderboard-userId">{props.user.id}</h4>
+                    <h3 className="leaderboard-userName">{user.name}</h3>
+                    <h4 className="leaderboard-userId">{user.id}</h4>
                 </div>
 
                 <div className="leaderboard-userAvatar-wrapper">
-                    <img className="leaderboard-userAvatar" src={props.user.avatarURL} alt="just an avatar"/>
+                    <img className="leaderboard-userAvatar" src={user.avatarURL} alt="just an avatar"/>
                 </div>
 
             </div>
-            <div className="leaderboard-user-answers leaderboard-item">{Object.keys(props.user.answers).length}</div>
-            <div className="leaderboard-user-created leaderboard-item">{props.user.questions.length}</div>
+            <div className="leaderboard-user-answers leaderboard-item">{Object.keys(user.answers).length}</div>
+            <div className="leaderboard-user-created leaderboard-item">{user.questions.length}</div>
         </div>
     )
 }
