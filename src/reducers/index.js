dcb05af8c07ea2eb2274dc5502568e7eb3798380
KenShyNoShana <ReneAslanov02@gmail.com>
2022-06-09 14:22:37 +0200

    feat: add new reducer to handle previous URL-location

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from "redux";
 import authedUser from "./authedUser";
 import userReducer from "./user";
 import questionReducer from "./questions";
+import locationReducer from "./location";
 
 export default combineReducers({
     authedUser,
     users: userReducer,
-    questions: questionReducer
+    questions: questionReducer,
+    location: locationReducer
 })
